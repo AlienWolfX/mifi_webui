9de143bfee1db5c71407de42305cb7025d5eaa87
Allen Cruiz <cruizallen2@gmail.com>
2024-05-25 09:18:20 +0800

    Update IT112.sh

--- IT112.sh
+++ IT112.sh
@@ -60,7 +60,10 @@ sudo exportfs -ar
 sudo exportfs -v
 
 # Firewall
+sudo ufw enable
 sudo ufw allow from $ip_prefix.0/24 to any port nfs
+sudo ufw allow ssh/tcp
+sudo ufw allow http/tcp
 sudo ufw status
 
 # Web
